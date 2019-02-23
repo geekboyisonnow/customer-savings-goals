8dc83aec5887d74f695a2c3dd85c8bb6161f1893
geekboyisonnow <kennethfcannon@gmail.com>
2019-02-23 00:58:10 -0500

    Transformed Arrow for fun.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './bank-ozk-logo.png';
 import plant from './plant.jpg'
+import arrow from './arrow-button.png'
 import './App.css';
 
 class App extends Component {
@@ -17,7 +18,7 @@ class App extends Component {
         VIEW MY GOALS
         </div>
         <div className="hover" id="nav-bkg">
-        ADD A NEW GOAL
+        ADD NEW GOALS
         </div>
         <div className="hover" id="nav-bkg">
         EDIT GOALS
@@ -27,8 +28,8 @@ class App extends Component {
         <div className="plant">
           <img src={plant} alt="plant planted in change jar"/>
         </div>
-        <div>
-          <h1>HERE IS LOREM IPSUM FOR START-UPS</h1>
+        <div className="arrow">
+          <h1>HERE IS LOREM IPSUM FOR START-UPS</h1><img src={arrow} alt="arrow button"/>
           <blockquote>
           Interaction design user experience long tail business plan stock social proof gamification series A financing scrum project churn rate incubator. Facebook technology first mover advantage gen-z scrum project strategy prototype metrics monetization bootstrapping supply chain innovator return on investment. Business-to-consumer customer hypotheses. Seed money facebook network effects release validation scrum project gen-z rockstar infrastructure iPad niche market.
           </blockquote>
