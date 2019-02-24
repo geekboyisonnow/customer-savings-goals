2d23eca80911dffdbfc653c3bc0310fd94b1e462
geekboyisonnow <kennethfcannon@gmail.com>
2019-02-24 17:21:41 -0500

    All Pages Created & Styled - Ready to Route

--- src/App.js
+++ src/App.js
@@ -30,7 +30,8 @@ class App extends Component {
         <div className="rainy-content">
           <h1>The Rainy Day Challenge</h1>
           <blockquote><p>Competition can be healthy.  So, try the Rainy Day Challenge.  Here's how it works... You pick a savings goal that you might need for a rainy day, such as; $100, $1,000, $10,000, or more.  Match your progress against others in the challenge.  See if you can beat expectations.  Are you ready for the Challenge?  Bank OZK helps you save today to meet your financial goals for tomorrow!</p></blockquote>
-        
+        </div>
+        <div className="content">
         <div className="row" id="progress">
           <div className="column">
           <label for="name" className="label"><strong>Your Goal:</strong></label>
@@ -38,20 +39,20 @@ class App extends Component {
             <div type="text" id="name" name="goal_name" className="label">The Rainy Day Challenge</div>
             </div>
           </div>
-          <div className="item-column">
-            <label for="target" className="label"><strong>Amount:</strong></label>
+          <div className="input-column">
+            <label for="target" className="label"><strong>Goal Amount:</strong></label>
             <div>
-            <div type="text" id="target" name="target_amount" className="label">$10,000.00</div>
+            <input type="number" min="0.01" step="0.01" max="" value="$10,000.00" id="target" name="target_amount" className="label" placeholder="$10,000.00"/>
             </div>
           </div>
-          <div className="item-column">
-            <label for="current" className="label"><strong>Balance:</strong></label>
+          <div className="input-column">
+            <label for="current" className="label"><strong>Current Balance:</strong></label>
             <div>
-            <div type="text" id="current" name="current_amount" className="label">$900.00</div>
+            <input type="number" min="0.01" step="0.01" max="" value="$300.00" id="current" name="current_amount" className="label" placeholder="$300.00"/>
             </div>
           </div>
           <div className="column">
-            <label for="date" className="label"><strong>Date:</strong></label>
+            <label for="date" className="label"><strong>Due Date:</strong></label>
             <div>
             <div type="date" id="date" name="target_date" className="label">01/01/2020</div>
             </div>
